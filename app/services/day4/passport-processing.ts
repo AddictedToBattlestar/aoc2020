82fd920a9f635adbd2d88a209c4f30892d39c314
John Ryan <john.ryan@cardinalhealth.com>
2020-12-04 13:04:05 -0500

    Solved part 2 for day 4

--- app/services/day4/passport-processing.ts
+++ app/services/day4/passport-processing.ts
@@ -2,14 +2,14 @@ import Service from '@ember/service';
 
 export default class Day4PassportProcessing extends Service.extend({
 }) {
-  public countValidPassports(passportData: Array<string>): number {
+  public countValidPassports(passportData: Array<string>, isUsingRevisedValidation: boolean): number {
     console.log(`Day 4, passportData: "${passportData}"`);
     let currentPassport: any = {};
     let validPassportCount = 0;
     for (let passportDataLine of passportData) {
       console.log(`Day 4, processing line: "${passportDataLine}"`);
       if (passportDataLine.length === 0) {
-        if (this.isPassportValid(currentPassport)) {
+        if (this.isPassportValid(currentPassport, isUsingRevisedValidation)) {
           validPassportCount++;
           console.log("passport valid");
         } else {
@@ -23,13 +23,26 @@ export default class Day4PassportProcessing extends Service.extend({
         }
       }
     }
-    if (this.isPassportValid(currentPassport)) {
+    if (this.isPassportValid(currentPassport, isUsingRevisedValidation)) {
       validPassportCount++;
     }
     return validPassportCount;
   }
 
-  private isPassportValid(passport: any) {
+  private isPassportValid(passport: any, isUsingRevisedValidation: boolean) {
+    debugger;
+    if (isUsingRevisedValidation) {
+      const isByrValid = passport["byr"] && this.isYearValid(passport["byr"], 1920, 2002);
+      const isIyrValid = passport["iyr"] && this.isYearValid(passport["iyr"], 2010, 2020);
+      const isEyrValid = passport["eyr"] && this.isYearValid(passport["eyr"], 2020, 2030);
+      const isHgtValid = passport["hgt"] && this.isHeightValid(passport["hgt"]);
+      const isHclValid = passport["hcl"] && this.isHairColorValid(passport["hcl"]);
+      const isEclValid = passport["ecl"] && this.isEyeColorValid(passport["ecl"]);
+      const isPidValid = passport["pid"] && this.isPassportIdValid(passport["pid"]);
+
+      return isByrValid && isIyrValid && isEyrValid && isHgtValid && isHclValid && isEclValid && isPidValid;
+      // && passport["cid"];
+    }
     return passport["byr"]
       && passport["iyr"]
       && passport["eyr"]
@@ -38,6 +51,42 @@ export default class Day4PassportProcessing extends Service.extend({
       && passport["ecl"]
       && passport["pid"];
     // && passport["cid"];
+  }
+
+  private isYearValid(stringValue: string, startingYear: number, endingYear: number): boolean {
+    if (stringValue.length !== 4) {
+      return false;
+    }
+    const value = Number(stringValue);
+    return endingYear >= value && value >= startingYear;
+  }
+
+  private isHeightValid(hgt: string): boolean {
+    if (hgt.length > 2 && hgt.substring(hgt.length - 2) === 'in') {
+      const height = Number(hgt.substring(0, hgt.length - 2));
+      return !isNaN(height) && 76 >= height && height >= 59;
+    }
+    if (hgt.length > 2 && hgt.substring(hgt.length - 2) === 'cm') {
+      const height = Number(hgt.substring(0, hgt.length - 2));
+      return !isNaN(height) && 193 >= height && height >= 150;
+    }
+    return false;
+  }
+
+  private isHairColorValid(hcl: string): boolean {
+    if (hcl.substring(0,1) !== "#" || hcl.length !== 7) {
+      return false;
+    }
+    var regExp = new RegExp(/^[a-f0-9]+$/);
+    return (regExp.test(hcl.substring(1)));
+  }
+
+  private isEyeColorValid(ecl: string): boolean {
+    return ecl === "amb" || ecl === "blu" || ecl === "brn" || ecl === "gry" || ecl === "grn" || ecl === "hzl" || ecl === "oth";
+  }
+
+  private isPassportIdValid(pid: string) {
+    return pid.length === 9 && !isNaN(Number(pid));
   }
 }
 
--- tests/unit/services/day4/passport-processing-test.ts
+++ tests/unit/services/day4/passport-processing-test.ts
@@ -10,13 +10,29 @@ module('Unit | Service | day4/passport-processing', function(hooks) {
   });
 
   test('it solves the example provided from aoc for part 1', function (assert) {
-    const result = service.countValidPassports(exampleData);
+    const result = service.countValidPassports(exampleData, false);
     assert.equal(result, 2);
   });
 
   test('it solves for part 1', function (assert) {
-    const result = service.countValidPassports(sourceData);
+    const result = service.countValidPassports(sourceData, false);
     console.log(`Day 4: The answer to part 1 is: ${result}\n`);
+    assert.ok(result);
+  });
+
+  test('it solves the 1st example provided from aoc for part 2', function (assert) {
+    const result = service.countValidPassports(exampleDataPart2a, true);
+    assert.equal(result, 0);
+  });
+
+  test('it solves the 2nd example provided from aoc for part 2', function (assert) {
+    const result = service.countValidPassports(exampleDataPart2b, true);
+    assert.equal(result, 4);
+  });
+
+  test('it solves for part 2', function (assert) {
+    const result = service.countValidPassports(sourceData, true);
+    console.log(`Day 4: The answer to part 2 is: ${result}\n`);
     assert.ok(result);
   });
 
@@ -34,6 +50,37 @@ module('Unit | Service | day4/passport-processing', function(hooks) {
 
   hcl:#cfa07d eyr:2025 pid:166559648
   iyr:2011 ecl:brn hgt:59in
+  `).trim().split("\n").map(l => l.trim());
+
+  const exampleDataPart2a: Array<string> = (`
+  eyr:1972 cid:100
+  hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+  iyr:2019
+  hcl:#602927 eyr:1967 hgt:170cm
+  ecl:grn pid:012533040 byr:1946
+
+  hcl:dab227 iyr:2012
+  ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+  hgt:59cm ecl:zzz
+  eyr:2038 hcl:74454a iyr:2023
+  pid:3556412378 byr:2007
+  `).trim().split("\n").map(l => l.trim());
+
+  const exampleDataPart2b: Array<string> = (`
+  pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+  hcl:#623a2f
+
+  eyr:2029 ecl:blu cid:129 byr:1989
+  iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+  hcl:#888785
+  hgt:164cm byr:2001 iyr:2015 cid:88
+  pid:545766238 ecl:hzl
+  eyr:2022
+
+  iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
   `).trim().split("\n").map(l => l.trim());
 
   const sourceData: Array<string> = (`
