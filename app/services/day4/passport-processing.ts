ea573b70e89da7ab463f8ac87cfbf3cfd740e068
AddictedToBattlestar <addictedtobsg@gmail.com>
2020-12-04 08:35:26 -0500

    Solved part 1 for day 4

--- app/services/day4/passport-processing.ts
+++ app/services/day4/passport-processing.ts
@@ -1,9 +1,44 @@
 import Service from '@ember/service';
 
 export default class Day4PassportProcessing extends Service.extend({
-  // anything which *must* be merged to prototype here
 }) {
-  // normal class body definition here
+  public countValidPassports(passportData: Array<string>): number {
+    console.log(`Day 4, passportData: "${passportData}"`);
+    let currentPassport: any = {};
+    let validPassportCount = 0;
+    for (let passportDataLine of passportData) {
+      console.log(`Day 4, processing line: "${passportDataLine}"`);
+      if (passportDataLine.length === 0) {
+        if (this.isPassportValid(currentPassport)) {
+          validPassportCount++;
+          console.log("passport valid");
+        } else {
+          console.log("passport NOT valid");
+        }
+        currentPassport = {};
+      } else {
+        for (let passportField of passportDataLine.split(" ")) {
+          const parsedPassportField = passportField.split(":");
+          currentPassport[parsedPassportField[0]] = parsedPassportField[1];
+        }
+      }
+    }
+    if (this.isPassportValid(currentPassport)) {
+      validPassportCount++;
+    }
+    return validPassportCount;
+  }
+
+  private isPassportValid(passport: any) {
+    return passport["byr"]
+      && passport["iyr"]
+      && passport["eyr"]
+      && passport["hgt"]
+      && passport["hcl"]
+      && passport["ecl"]
+      && passport["pid"];
+    // && passport["cid"];
+  }
 }
 
 // DO NOT DELETE: this is how TypeScript knows how to look up your services.
