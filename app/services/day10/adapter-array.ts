842edb742cc04a6c39b73ceddfabe37765059416
AddictedToBattlestar <addictedtobsg@gmail.com>
2020-12-15 19:47:26 -0500

    wip on part 2

--- app/services/day10/adapter-array.ts
+++ app/services/day10/adapter-array.ts
@@ -10,14 +10,19 @@ export default class Day10AdapterArray extends Service.extend({
       3: 0
     };
     const adapterListing: Array<number> = rawAdapterListing.map(a => Number(a)).sort((a1, a2) => a1 - a2);
+    let debugLog = "*";
+    let adapterLog = "(0),";
     for(let i = 0; i < adapterListing.length; i++) {
       const joltageDifference = i === 0 ? adapterListing[i] : adapterListing[i] - adapterListing[i - 1];
       if (joltageDifference > 3) {
         throw `An adapter could not be found to join to ${adapterListing[i - 1]}`;
       }
-      console.debug(`Adapter ${adapterListing[i]} added with a joltage difference of ${joltageDifference}`);
+      debugLog += `${joltageDifference}*`;
+      adapterLog += `${adapterListing[i]},`;
       joltageDifferenceCounter[joltageDifference]++;
     }
+    console.debug(`${debugLog}3*`);
+    console.debug(`${adapterLog}(${adapterListing[adapterListing.length - 1] + 3})`);
     joltageDifferenceCounter[3]++;
     return {
       oneJoltDifferenceCount: joltageDifferenceCounter[1],
